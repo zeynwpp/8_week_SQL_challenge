069dc046bf753101175c3d8e6b73574d54cf683b
Zeynep Dönmez <84280282+zeynwpp@users.noreply.github.com>
2025-03-11 00:15:52 +0300

    Update Week_1_DannysDinner.sql

--- Week_1_DannysDinner.sql
+++ Week_1_DannysDinner.sql
@@ -43,10 +43,36 @@ group by m.product_name
 
 
 -- 5. Which item was the most popular for each customer?
+with purchase_count as (
+    select
+        s.customer_id,
+        s.product_id,
+        COUNT(*) AS order_count,
+        DENSE_RANK() OVER (PARTITION BY s.customer_id ORDER BY COUNT(*) desc) AS rnk
+    from dannys_diner.sales s
+    group by s.customer_id, s.product_id
+)
 
+select pc.customer_id, 
+       m.product_name, 
+       pc.order_count
+from purchase_count pc
+INNER JOIN dannys_diner.menu m on m.product_id = pc.product_id  
+where pc.rnk = 1; 
 
 -- 6. Which item was purchased first by the customer after they became a member?
+
+
+
 -- 7. Which item was purchased just before the customer became a member?
+
+
 -- 8. What is the total items and amount spent for each member before they became a member?
+
+
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
+
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+
+
